8a21f21a61e8dd3920aba1ef4f030fca9819f34c
Nicolás Potier <nicolaspotier97@gmail.com>
2021-01-04 13:46:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This app stay on development
+        </p>
+        <a
+          className="App-link"
+          href="https://nipoanz.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit my webSite
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
